4073437e8181f673bb1e87b0ab5e5375bb24204a
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-01-08 12:26:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/movie_app_auth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.movie_app_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_auth/features/authentication/presentation/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
